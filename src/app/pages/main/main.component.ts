52d444d0ae9ba738897d2c8914545659234b9a9a
Eddie <ggordon@email.sc.edu>
2019-08-14 21:23:42 -0400

    Image Download (Client Side)

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Ng2ImgMaxService } from 'ng2-img-max';
 import { DomSanitizer } from '@angular/platform-browser';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-main',
@@ -9,13 +10,17 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class MainComponent {
 
-  image50x50: File;
-  image150x150: File;
-  image250x250: File;
+  image28x28: File;
+  image56x56: File;
+  image112x112: File;
 
-  imagePreview50x50: string;
-  imagePreview150x150: string;
-  imagePreview250x250: string;
+  imagePreview28x28: string;
+  imagePreview56x56: string;
+  imagePreview112x112: string;
+
+  fileUrl28: any;
+  fileUrl56: any;
+  fileUrl112: any;
 
   reader: FileReader = new FileReader();
 
@@ -26,51 +31,57 @@ export class MainComponent {
 
   resizeImage(event) {
     const image = event.target.files[0];
-    this.ng2ImgMax.resizeImage(image, 50, 50).subscribe(
+    this.ng2ImgMax.resizeImage(image, 28, 28).subscribe(
       result => {
-        this.image50x50 = new File([result], result.name);
-        this.getImagePreview50(this.image50x50);
+        this.image28x28 = new File([result], result.name);
+        const blob28 = new Blob([this.image28x28]);
+        this.fileUrl28 = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob28));
+        this.getImagePreview50(this.image28x28);
       },
       error => {
-        console.log('Error 50x50', error);
+        console.log('Error 28x28', error);
       }
     );
-    this.ng2ImgMax.resizeImage(image, 150, 150).subscribe(
+    this.ng2ImgMax.resizeImage(image, 56, 56).subscribe(
       result => {
-        this.image150x150 = new File([result], result.name);
-        this.getImagePreview150(this.image150x150);
+        this.image56x56 = new File([result], result.name);
+        const blob56 = new Blob([this.image56x56]);
+        this.fileUrl56 = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob56));
+        this.getImagePreview150(this.image56x56);
       },
       error => {
-        console.log('Error 150x150', error);
+        console.log('Error 56x56', error);
       }
     );
-    this.ng2ImgMax.resizeImage(image, 250, 250).subscribe(
+    this.ng2ImgMax.resizeImage(image, 112, 112).subscribe(
       result => {
-        this.image250x250 = new File([result], result.name);
-        this.getImagePreview250(this.image250x250);
+        this.image112x112 = new File([result], result.name);
+        const blob112 = new Blob([this.image112x112]);
+        this.fileUrl112 = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob112));
+        this.getImagePreview250(this.image112x112);
       },
       error => {
-        console.log('Error 250x250', error);
+        console.log('Error 112x112', error);
       }
     );
   }
 
-  getImagePreview50(file50: File) {
-    this.reader.readAsDataURL(file50);
+  getImagePreview50(file28: File) {
+    this.reader.readAsDataURL(file28);
     this.reader.onload = () => {
-      this.imagePreview50x50 = this.reader.result.toString();
+      this.imagePreview28x28 = this.reader.result.toString();
     };
   }
-  getImagePreview150(file150: File) {
-    this.reader.readAsDataURL(file150);
+  getImagePreview150(file56: File) {
+    this.reader.readAsDataURL(file56);
     this.reader.onload = () => {
-      this.imagePreview150x150 = this.reader.result.toString();
+      this.imagePreview56x56 = this.reader.result.toString();
     };
   }
-  getImagePreview250(file250: File) {
-    this.reader.readAsDataURL(file250);
+  getImagePreview250(file112: File) {
+    this.reader.readAsDataURL(file112);
     this.reader.onload = () => {
-      this.imagePreview250x250 = this.reader.result.toString();
+      this.imagePreview112x112 = this.reader.result.toString();
     };
   }
 }

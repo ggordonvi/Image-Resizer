79eae6292dc627aaf69bd336175737ccdeaec334
Eddie <ggordon@email.sc.edu>
2019-08-15 01:03:06 -0400

    Clean Up (Variables to Arrays, Generalized Func)

--- README.md
+++ README.md
@@ -9,5 +9,3 @@ Current implementation takes image and resizes it to 28 x 28, 56 x 56, and 112 x
 Ng2ImgMaxService - https://www.npmjs.com/package/ng2-img-max
 <br>
 DomSanitizer - https://angular.io/api/platform-browser/DomSanitizer
-<br>
-fileSaver - https://www.npmjs.com/package/file-saver
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Ng2ImgMaxService } from 'ng2-img-max';
 import { DomSanitizer } from '@angular/platform-browser';
-import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-main',
@@ -10,17 +9,10 @@ import { saveAs } from 'file-saver';
 })
 export class MainComponent {
 
-  image28x28: File;
-  image56x56: File;
-  image112x112: File;
-
-  imagePreview28x28: string;
-  imagePreview56x56: string;
-  imagePreview112x112: string;
-
-  fileUrl28: any;
-  fileUrl56: any;
-  fileUrl112: any;
+  image: File[] = [];
+  imagePreview: string[] = [];
+  fileUrl: any[] = [];
+  blob: any[] = [];
 
   reader: FileReader = new FileReader();
 
@@ -33,10 +25,10 @@ export class MainComponent {
     const image = event.target.files[0];
     this.ng2ImgMax.resizeImage(image, 28, 28).subscribe(
       result => {
-        this.image28x28 = new File([result], result.name);
-        const blob28 = new Blob([this.image28x28]);
-        this.fileUrl28 = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob28));
-        this.getImagePreview50(this.image28x28);
+        this.image[0] = new File([result], result.name);
+        this.blob[0] = new Blob([this.image[0]]);
+        this.fileUrl[0] = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(this.blob[0]));
+        this.getImagePreview(this.image[0], 0);
       },
       error => {
         console.log('Error 28x28', error);
@@ -44,10 +36,10 @@ export class MainComponent {
     );
     this.ng2ImgMax.resizeImage(image, 56, 56).subscribe(
       result => {
-        this.image56x56 = new File([result], result.name);
-        const blob56 = new Blob([this.image56x56]);
-        this.fileUrl56 = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob56));
-        this.getImagePreview150(this.image56x56);
+        this.image[1] = new File([result], result.name);
+        this.blob[1] = new Blob([this.image[1]]);
+        this.fileUrl[1] = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(this.blob[1]));
+        this.getImagePreview(this.image[1], 1);
       },
       error => {
         console.log('Error 56x56', error);
@@ -55,10 +47,10 @@ export class MainComponent {
     );
     this.ng2ImgMax.resizeImage(image, 112, 112).subscribe(
       result => {
-        this.image112x112 = new File([result], result.name);
-        const blob112 = new Blob([this.image112x112]);
-        this.fileUrl112 = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob112));
-        this.getImagePreview250(this.image112x112);
+        this.image[2] = new File([result], result.name);
+        this.blob[2] = new Blob([this.image[2]]);
+        this.fileUrl[2] = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(this.blob[2]));
+        this.getImagePreview(this.image[2], 2);
       },
       error => {
         console.log('Error 112x112', error);
@@ -66,22 +58,18 @@ export class MainComponent {
     );
   }
 
-  getImagePreview50(file28: File) {
-    this.reader.readAsDataURL(file28);
+  getImagePreview(file: File, num: number) {
+    this.reader.readAsDataURL(file);
     this.reader.onload = () => {
-      this.imagePreview28x28 = this.reader.result.toString();
+      this.imagePreview[num] = this.reader.result.toString();
     };
   }
-  getImagePreview150(file56: File) {
-    this.reader.readAsDataURL(file56);
-    this.reader.onload = () => {
-      this.imagePreview56x56 = this.reader.result.toString();
-    };
-  }
-  getImagePreview250(file112: File) {
-    this.reader.readAsDataURL(file112);
-    this.reader.onload = () => {
-      this.imagePreview112x112 = this.reader.result.toString();
-    };
+
+  checkForImage() {
+    if (this.imagePreview[0] == null || this.imagePreview[1] == null || this.imagePreview[2] == null) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
